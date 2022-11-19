19899e23e449d5cab268638b378cdd310bb6d4de
Stephen Byrne <62932312+stephen447@users.noreply.github.com>
2022-11-19 16:27:27 +0000

    Create Structs.cpp

---
+++ Classes/Structs.cpp
@@ -0,0 +1,15 @@
+struct Student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
